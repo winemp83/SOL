184b8d375012ff7bf26d3d770ed1b359933c3066
winemp83 <koehne83@googlemail.com>
2013-09-21 13:40:29 +0200

    delete double entry in ingame

---
+++ includes/pages/game/class.ShowAllianzForumPage.php
@@ -0,0 +1,19 @@
+<?php
+class ShowAlliBankPage extends AbstractPage 
+{
+	public static $requireModule = MODULE_SUPPORT;
+	
+
+	function __construct() 
+	{
+		parent::__construct();
+		
+	}
+		
+	function show()
+	{
+		
+	}
+
+}
+	
