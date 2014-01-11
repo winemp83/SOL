c07eb801a032b46256283bca0936b31d5658c3bf
Sascha <Sascha@Arbeit>
2014-01-11 12:56:47 +0100

    change the fleettrader he will now random change what for ships he recive

--- includes/classes/cronjob/MarketCronjob.class.php
+++ includes/classes/cronjob/MarketCronjob.class.php
@@ -10,12 +10,14 @@
 
 class MarketCronjob {
     public function run (){
+        
         $i = 5;
         for($j = 0; $j < $i; $j++){
             //$this->insertAuction();
             
            $this->insertAuction();
         }
+        $this->updateFleetTrader();
     }
     function insertAuction(){
         $GLOBALS['DATABASE']->query($this->prepareAuction());
@@ -136,5 +138,84 @@ class MarketCronjob {
         $a = mt_rand(0,3);
         $b = mt_rand(1,2);
        return $this->auction($a,$b);
+    }
+    
+    function rndShips(){
+        $a = mt_rand(1,15);
+        return $a;
+    }
+    function rndDef(){
+        $a = mt_rand(1,7);
+        return $a;
+    }
+    function getShips($a){
+        switch($a){
+            case 1  : return 202;
+                      break;
+            case 2  : return 203;
+                      break;
+            case 3  : return 204;
+                      break;
+            case 4  : return 205;
+                      break;
+            case 5  : return 206;
+                      break;
+            case 6  : return 207;
+                      break;
+            case 7  : return 209;
+                      break;
+            case 8  : return 211;
+                      break;
+            case 9  : return 213;
+                      break;
+            case 10  : return 214;
+                      break;
+            case 11  : return 215;
+                      break;
+            case 12  : return 216;
+                      break;
+            case 13  : return 217;
+                      break;
+            case 14  : return 218;
+                      break;
+            case 15  : return 219;
+                      break;
+        }
+    }
+    function getDef($a){
+        switch ($a){
+            case 1 : return 401;
+                break;
+            case 2 : return 402;
+                break;
+            case 3 : return 403;
+                break;
+            case 4 : return 404;
+                break;
+            case 5 : return 405;
+                break;
+            case 6 : return 406;
+                break;
+            case 7 : return 410;
+                break;
+        }
+    }
+    function createArt(){
+        $a = $this->rndDef();
+        $b = 0;
+        $c = 0;
+        while($b === $c){
+            $b = $this->rndShips();
+            $c = $this->rndShips();
+        }
+        $ba = $this->getShips($b);
+        $bb = $this->getShips($c);
+        $bc = $this->getDef($a);
+        $result = $ba.",".$bb.",".$bc;
+        return $result;
+    }
+    function updateFleetTrader(){
+        $sql = "UPDATE uni1_config SET trade_allowed_ships='".$this->createArt()."'";
+        $GLOBALS['DATABASE']->query($sql);
     }
 }
